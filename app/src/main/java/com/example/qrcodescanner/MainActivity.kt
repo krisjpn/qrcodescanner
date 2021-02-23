956710a1b9494d03b5da8150789a6579fa65237b
Kris <fauszk@gmail.com>
2021-02-23 16:43:54 +0900

    Add data save function

--- app/src/main/java/com/example/qrcodescanner/MainActivity.kt
+++ app/src/main/java/com/example/qrcodescanner/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.zxing.integration.android.IntentIntegrator
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,9 +16,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         btn_scan.setOnClickListener {
 
+
+
+
             val scanner = IntentIntegrator(this)
 
-            scanner.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+            //scanner.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
             scanner.setBeepEnabled(true)
             scanner.initiateScan()
 
@@ -28,16 +32,48 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
 
         if (resultCode == Activity.RESULT_OK) {
-            val result =
+            val barcodeData =
                 IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
 
-            if (result != null) {
+            if (barcodeData != null) {
 
-                if (result.contents == null) {
+                if (barcodeData.contents == null) {
                     Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
                 } else {
-                    Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG)
+                    Toast.makeText(this, "Scanned: " + barcodeData.contents, Toast.LENGTH_LONG)
                         .show()
+
+                    //show product details
+
+
+                    //or save new data to database
+                    val context = applicationContext
+                    val filename = "barcodeDatabase"
+                    val file = File(context.filesDir, filename)
+
+
+                    if (file.exists()) {
+
+                        val data = file.readText()
+
+                        if (data == barcodeData.contents)
+                        {
+                            Toast.makeText(this, "Data exists. " + barcodeData.contents, Toast.LENGTH_LONG)
+                                .show()
+                        }
+                        else
+                        {
+                            file.writeText(barcodeData.contents)
+                            Toast.makeText(this, "Data saved. ", Toast.LENGTH_LONG)
+                                .show()
+                        }
+                    }
+                    else
+                    {
+                        file.createNewFile()
+                        Toast.makeText(this, "New File Created. ", Toast.LENGTH_LONG)
+                            .show()
+                    }
                 }
             } else {
                 super.onActivityResult(requestCode, resultCode, data)
